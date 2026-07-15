93b9300ab0adde4930bebc8f82c15dcdfa188c3f
melissavillegas251-arch <melissavillegas251@gmail.com>
2026-07-15 13:40:19 -0500

    feat: update js

--- DEW_Tema_3/DEW_Tema_03/Ejercicio_1/js/script.js
+++ DEW_Tema_3/DEW_Tema_03/Ejercicio_1/js/script.js
@@ -1,9 +1,11 @@
 function fecha() {
-    new Date();
+
     var fecha = new Date();
     var dia = fecha.getDate();
     var mes = fecha.getMonth() + 1;
     var anio = fecha.getFullYear();
+
+    var anioStr = String(anio).slice(-2);
 
 alert("Hoy es " + dia + "/" + mes + "/" + anio);
 
